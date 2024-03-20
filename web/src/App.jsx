6642d6422f1571e7cc4f0dbb7adfbed7b0585bf9
Henry Wu <hhwu@college.harvard.edu>
2024-03-20 18:00:24 -0400

    Add plots to website

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Plot from 'react-plotly.js'
+import choroplethJSON from '../../choropleth.json';
+import choroplethscatterJSON from '../../choropleth_with_scatter.json';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Plot data={choroplethJSON.data} layout={choroplethJSON.layout}/>
     </>
   )
 }
