7b07c7f777dedeea3239fb6df39c386ceb81f448
Conan Lu <Conan Lu>
2024-04-25 15:56:25 -0400

    image optimization

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  ViteImageOptimizer({
+    /* pass your config */
+  })],
 })
